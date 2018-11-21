a3903c0ca74018f6f455b3b1ef87ee97dfdb1a3f
ANIL JOSEPH <aniljoseph007@gmail.com>
2018-11-22 00:47:21 +0530

    Basic XML Parsing works

--- CppQtConsole/main.cpp
+++ CppQtConsole/main.cpp
@@ -1,21 +1,66 @@
 #include <QCoreApplication>
 #include "baseclass.h"
 #include <iostream>
+#include <QFile>
+#include <QDebug>
+#include <QXmlStreamReader>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    qDebug()<<"App Started";
     QCoreApplication a(argc, argv);
 
-    /* Custom Code */
-    employeeBase empBase1(12);
+    QFile file("C:/Users/anilj/OneDrive/Desktop/XML/TestXML.xml");
+    if(!file.open(QFile::ReadOnly | QFile::Text)){
+        qDebug() << "Cannot read file" << file.errorString();
+        exit(0);
+    }
 
-    employeeBase empBase2 =empBase1;
+    QXmlStreamReader XMLData(&file);
 
-    displayAge(empBase1);
-    displayAge(empBase2);
+    while(!XMLData.atEnd() && !XMLData.hasError())
+    {
+        XMLData.readNext();
+        if(XMLData.isStartElement())
+        {
+            qDebug() << XMLData.name();
+        }
+    }
 
-    cout<<"Before exit";
+
+
+#if 0
+    if (reader.readNextStartElement())
+    {
+        if (reader.name() == "Root")
+        {
+            qDebug()<<"Parsed 'Root' element";
+            reader.readNextStartElement();
+            if(reader.name() == "Book1")
+            {
+                while (!reader.isEndDocument())
+                {
+                    if(reader.isStartElement())
+                    {
+                        qDebug()<<reader.name();
+                    }
+                    else if(reader.isEndElement())
+                    {
+                        reader.readNext();
+                    }
+                }
+            }
+        }
+        else
+        {
+            reader.raiseError(QObject::tr("Incorrect file"));
+        }
+    }
+#endif
+
     return a.exec();
+    //return 0;
 }
+
